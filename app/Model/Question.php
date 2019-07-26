25205085092a38c1eca72495589854fb9e46dbe8
Yazidane <yazidane2010@gmail.com>
2019-07-26 12:42:59 +0800

    Relationship

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user(){
+    	$this->belongsTo(User::class);
+    }
+
+    public function replies(){
+    	$this->hasMany(Reply::class);
+    }
+
+    public function category(){
+    	$this->belongsTo(Category::class);
+    }
+
 }
